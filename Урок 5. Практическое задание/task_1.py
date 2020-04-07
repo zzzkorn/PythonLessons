fedb3242fce6840076190805db86b901a69fa9bb
Pavel <PavelEvseev92@yandex.ru>
2020-04-07 17:04:17 +0300

    Complete

--- Урок 5. Практическое задание/task_1.py
+++ Урок 5. Практическое задание/task_1.py
@@ -25,3 +25,153 @@
 
 Предприятия, с прибылью ниже среднего значения: Копыта
 """
+from collections import namedtuple
+from collections import defaultdict
+from collections import Counter
+from functools import reduce
+
+
+def fill_enterprises_namedtuple(enterprises, number_of_enterprises):
+    if number_of_enterprises:
+        try:
+            name = input("Введите название предприятия: ")
+            quarterly_profit = list(map(int, str(input(
+                "Через пробел введите прибыль данного предприятия за каждый "
+                "квартал(Всего 4 квартала): ")).split(' ')))
+            if len(quarterly_profit) == 4:
+                year_profit = sum(quarterly_profit)
+                enterprise = namedtuple('enterprise',
+                                        'name quarterly_profit year_profit')
+                ent = enterprise(
+                    name=name,
+                    quarterly_profit=quarterly_profit,
+                    year_profit=year_profit
+                )
+                enterprises.append(ent)
+                return fill_enterprises_namedtuple(
+                    enterprises, number_of_enterprises - 1)
+            else:
+                print('Должна быть введена прибыль за 4 квартала, попробуйте еще раз!')
+                return fill_enterprises_namedtuple(
+                    enterprises, number_of_enterprises)
+        except ValueError:
+            print('Ошибка ввода, попробуйте еще раз!')
+            return fill_enterprises_namedtuple(
+                enterprises, number_of_enterprises)
+    else:
+        return sum(i.year_profit for i in enterprises) / len(enterprises)
+        # return reduce(lambda x, y: x.year_profit + y.year_profit,
+        # enterprises) / len(enterprises) - НЕ РАБОТАЕТ!!!!
+
+
+def fill_enterprises_default_dict(enterprises, number_of_enterprises):
+    if number_of_enterprises:
+        try:
+            name = input("Введите название предприятия: ")
+            print(
+                "Введите прибыль данного предприятия за каждый квартал(Всего 4 квартала): ")
+            for i in range(1, 5):
+                profit = (name, int(input(f"{i} квартал: ")))
+                enterprises.append(profit)
+            return fill_enterprises_default_dict(
+                enterprises, number_of_enterprises - 1)
+        except ValueError:
+            print('Ошибка ввода, попробуйте еще раз!')
+            return fill_enterprises_default_dict(
+                enterprises, number_of_enterprises)
+    else:
+        ent = defaultdict(list)
+        for k, v in enterprises:
+            ent[k].append(v)
+        print(ent)
+        averaged_profit = sum(ent.values()) / len(ent)
+        return ent, averaged_profit
+
+
+def fill_enterprises_counter(enterprises, number_of_enterprises):
+    if number_of_enterprises:
+        try:
+            name = input("Введите название предприятия: ")
+            print(
+                "Введите прибыль данного предприятия за каждый квартал(Всего 4 квартала): ")
+            for i in range(1, 5):
+                for j in range(int(input(f"{i} квартал: "))):
+                    enterprises.append(name)
+            return fill_enterprises_counter(
+                enterprises, number_of_enterprises - 1)
+        except ValueError:
+            print('Ошибка ввода, попробуйте еще раз!')
+            return fill_enterprises_counter(enterprises, number_of_enterprises)
+    else:
+        ent = Counter(enterprises)
+        averaged_profit = sum(ent.values()) / len(ent)
+        return ent, averaged_profit
+
+
+def task1_namedtuple():
+    try:
+        number_of_enterprises = int(
+            input('Введите количество предприятий для расчета прибыли: '))
+        enterprises = []
+        averaged_profit = fill_enterprises_namedtuple(
+            enterprises, number_of_enterprises)
+        print(f'Средняя годовая прибыль всех предприятий: {averaged_profit}')
+        print("Предприятия, с прибылью выше среднего значения:")
+        for i in filter(
+                lambda x: x.year_profit > averaged_profit,
+                enterprises):
+            print(i.name)
+        print("Предприятия, с прибылью ниже среднего значения:")
+        for i in filter(
+                lambda x: x.year_profit <= averaged_profit,
+                enterprises):
+            print(i.name)
+    except ValueError:
+        print('Ошибка ввода количества предприятий!')
+
+
+def task1_default_dict():
+    try:
+        number_of_enterprises = int(
+            input('Введите количество предприятий для расчета прибыли: '))
+        enterprises = []
+        enterprises, averaged_profit = fill_enterprises_default_dict(
+            enterprises, number_of_enterprises)
+        print(f'Средняя годовая прибыль всех предприятий: {averaged_profit}')
+        print("Предприятия, с прибылью выше среднего значения:")
+        for i in {x: y for x, y in enterprises.items() if y > averaged_profit}:
+            print(i)
+        print("Предприятия, с прибылью ниже среднего значения:")
+        for i in {x: y for x, y in enterprises.items() if y <= averaged_profit}:
+            print(i)
+    except ValueError:
+        print('Ошибка ввода количества предприятий!')
+
+
+def task1_counter():
+    try:
+        number_of_enterprises = int(
+            input('Введите количество предприятий для расчета прибыли: '))
+        enterprises = []
+        enterprises, averaged_profit = fill_enterprises_counter(
+            enterprises, number_of_enterprises)
+        print(f'Средняя годовая прибыль всех предприятий: {averaged_profit}')
+        print("Предприятия, с прибылью выше среднего значения:")
+        for i in {x: y for x, y in enterprises.items() if y > averaged_profit}:
+            print(i)
+        print("Предприятия, с прибылью ниже среднего значения:")
+        for i in {x: y for x, y in enterprises.items() if y <=
+                  averaged_profit}:
+            print(i)
+    except ValueError:
+        print('Ошибка ввода количества предприятий!')
+
+
+def main():
+    task1_namedtuple()
+    task1_default_dict()
+    task1_counter()
+
+
+main()
+
--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -11,3 +11,58 @@
 Также попробуйте решить задачу вообще без collections и применить только ваши знания по ООП
 (в частности по перегрузке методов)
 """
+from collections import deque
+
+
+class UserNumber:
+    def __init__(self, number):
+        self.number = number
+
+    def __add__(self, other):
+        try:
+            return UserNumber.get_16_to_list(
+                int(self.number, 16) + int(other.number, 16))
+        except ValueError:
+            print("Ошибка ввода 16-ричного числа")
+
+    def __mul__(self, obj):
+        try:
+            return UserNumber.get_16_to_list(
+                int(self.number, 16) * int(obj.number, 16))
+        except ValueError:
+            print("Ошибка ввода 16-ричного числа")
+
+    @staticmethod
+    def get_one_number_16_to_str(number):
+        if number < 10:
+            return str(number)
+        elif number == 10:
+            return 'A'
+        elif number == 11:
+            return 'B'
+        elif number == 12:
+            return 'C'
+        elif number == 13:
+            return 'D'
+        elif number == 14:
+            return 'E'
+        elif number == 15:
+            return 'F'
+
+    @staticmethod
+    def get_16_to_list(number):
+        return_list = deque()
+        while number:
+            return_list.appendleft(UserNumber.get_one_number_16_to_str(number % 16))
+            number = number // 16
+        return return_list
+
+
+def task_2_class():
+    num_1 = UserNumber(input('Введите первое 16-ричное число: '))
+    num_2 = UserNumber(input('Введите второе 16-ричное число: '))
+    print(f'Сумма: {num_1 + num_2}')
+    print(f'Произведение: {num_1 * num_2}')
+
+
+task_2_class()
